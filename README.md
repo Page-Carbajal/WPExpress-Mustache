1e9d3f93dd5760fec17e96c749ac91d4cd9a7a40
awesomepage <page@iconicamarketing.com>
2016-09-10 00:23:05 -0500

    Documentation update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # WPExpress Mustache
+
+Since **WPExpress** version 1.4.0. **WPExpress/UI** was deprecated. 
+ 
+All necessary files for **WPExpress/UI** were assimilated into the core and the support for **WPExpress/UI** project was therefore terminated.
+   
+The last version of [WPExpress/UI](https://packagist.org/packages/page-carbajal/wpexpress-ui) is available on packagist and also available on [GitHub](https://github.com/Page-Carbajal/WPExpress-UI).
+
+I went into great details about my pain on [terminating the WPExpress/UI](http://pagecarbajal.com/a-minimal-footprint/) this project on my blog. My greatest concern was to give up on templating engines. 
+
+However I decided to be a grown up and not to force my vision into the world. I keep my commitment to provide useful tool for development. But I also need to get some satisfaction on coding for me.
+
+This project allows me and others to use Mustache as RenderEngine. 
+
+
+##Change Log
+
+###Version 1.0.0
+
+- Basic Mustache Implementation
---
+++ source/RenderEngine/Mustache.class.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace RenderEngine;
+
+
+use Mustache_Engine;
+use Mustache_Loader_FilesystemLoader;
+use WPExpress\UI\BaseRenderEngine;
+
+
+class Mustache extends BaseRenderEngine
+{
+
+    public function __construct( $context = false )
+    {
+        parent::__construct($context);
+
+        $this->templatePath  = 'templates/mustache';
+        $this->typeExtension = 'mustache';
+    }
+
+    public function render( $filename, $context = false )
+    {
+        $this->setContext($context);
+
+        $templateDirectory = $this->getTemplateDirectory();
+
+        // Include file or notify error
+        if( file_exists($templateDirectory) ) {
+            $options            = array();
+            $options['loader']  = new Mustache_Loader_FilesystemLoader($templateDirectory);
+            $options['charset'] = 'UTF-8';
+
+            $mustache = new Mustache_Engine($options);
+
+            return $mustache->render($this->getFileName($filename), $context);
+
+        } else {
+            // If not template was found then display the message
+            return $this->getTemplateNotFoundMessage();
+        }
+    }
+}
